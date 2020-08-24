b77f8795960a35c34afa4022c72c1e0f54fc4ceb
Leslie Chung <hklchung@gmail.com>
2020-08-24 23:00:40 +1000

    Func to extract quote from finnhub

---
+++ 2020/main.py
@@ -0,0 +1,42 @@
+"""
+Copyright (c) 2020, Heung Kit Leslie Chung
+All rights reserved.
+Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+1. Redistributions of source code must retain the above copyright notice, this
+    list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+3. Neither the name of the copyright holder nor the names of its contributors
+    may be used to endorse or promote products derived from this software
+    without specific prior written permission.
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+    AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+    IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+    ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+    LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+    CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+    SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+    INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+    CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+    ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+    POSSIBILITY OF SUCH DAMAGE.
+"""
+
+import requests
+import pandas as pd
+import numpy as np
+
+symbol = 'AAPL'
+resolution = 1
+
+def request_quote(symbol, resolution, token):
+    q_string = 'https://finnhub.io/api/v1/stock/candle?symbol={}&resolution={}&from=1572651390&to=1572910590&token={}'
+    r = requests.get(q_string.format(symbol, resolution, token))
+    c = []
+    [c.append(x) for x in r.json().keys()]
+    df = pd.DataFrame.from_dict(r.json(), orient='index').reset_index().T[1:]
+    df.columns = c
+    del(df['s'])
+    return df
